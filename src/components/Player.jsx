d95f118ac5dde9a58d91215cd0424e9fee54ae96
Kaká Xavier Sena <kakaxsena@gmail.com>
2025-06-04 23:35:08 -0300

    creating state to handle player name click

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,11 +1,21 @@
+import { useState } from "react";
 export default function Player({ name, symbol }) {
+  const [isEditing, setIsEditing] = useState(false);
+
+  function handleEditClick() {
+    setIsEditing(true);
+  }
   return (
     <li>
       <span className="player">
-        <span className="player-name">{name}</span>
+        {isEditing ? (
+          <input type="text" required />
+        ) : (
+          <span className="player-name">{name}</span>
+        )}
         <span className="player-symbol">{symbol}</span>
       </span>
-      <button>edit</button>
+      <button onClick={handleEditClick}>edit</button>
     </li>
   );
 }
